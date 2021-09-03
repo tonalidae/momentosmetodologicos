dfbabac828fd8c9867f4f8386e56018732bd0453
Stephanie Carolina Cely Rodriguez <76001312+CiudadUnida@users.noreply.github.com>
2021-09-03 10:32:58 -0600

    xddd KHE ESTA PASANDOO

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var content = 'El viaje apareció en medio de sueños sin forma, de ideas sin ma
 var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
 
 
-$(ele).hide().appendTo('p').each(function (i) {
+$(ele).hide().appendTo('.p1').each(function (i) {
     $(this).delay(100 * i).css({
         display: 'inline',
         opacity: 0
