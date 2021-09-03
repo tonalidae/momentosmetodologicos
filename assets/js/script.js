419ec7e605e3988df2138e79469a4a13a7a62643
Stephanie Carolina Cely Rodriguez <76001312+CiudadUnida@users.noreply.github.com>
2021-09-03 06:18:13 -0600

    prueba-animaciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-const checkpoint = 300;
-
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll <= checkpoint) {
-    opacity = 1 - currentScroll / checkpoint;
-  } else {
-    opacity = 0;
-  }
-  document.querySelector(".front").style.opacity = opacity;
-});
+AOS.init({
+  duration: 1200,
+})
